1d48fad39a316210728548d4a503358913e69ddf
Yosef Muluneh <119527329+yosefMuluneh@users.noreply.github.com>
2025-12-22 13:44:39 +0300

    setup tailwind, daisyui, icons (#49)

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,10 @@
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <h1 class="text-2xl font-bold">
+      Welcome to Nuxt 4 with daisyUI!
+    </h1>
+    <button class="btn btn-primary mt-4">
+      Click Me <Icon name="tabler:map" />
+    </button>
   </div>
 </template>
